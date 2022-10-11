ada60ca3b46fce479b3024377e49afd38f6fe546
Simge Bilgisayar <Simge Bilgisayar@DESKTOP-V8I8313.local>
2022-10-11 13:37:55 +0300

    deneme

---
+++ Kodlama.io/Day2/classes/Main.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //classes are reference types
+        customerManager customerManager = new customerManager();
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+
+    }
+}
---
+++ Kodlama.io/Day2/classes/customerManager.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class customerManager {
+    public void Add() {
+        System.out.println("Customer is added");
+    }
+
+    public void Remove() {
+        System.out.println("Customer is removed");
+    }
+
+    public void Update() {
+        System.out.println("Customer is updated");
+    }
+
+}
